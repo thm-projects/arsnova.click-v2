2a5c683a4d1a8c4ab2e24576f4e234fec2eea5ee
Christopher Fullarton <christopher@fullarton.eu>
2018-06-10 09:36:11 +0200

    Fixes style issues in the footer move icon and the connection indicator on mobile devices

--- src/lib/default.settings.ts
+++ src/lib/default.settings.ts
@@ -4,10 +4,10 @@ const environmentData = require(`../assets/serverEndpoint.json`);
 
 export const DefaultSettings = {
   siteId: 'arsnova.click-v2',
-  httpApiEndpoint: environmentData.httpApiEndpoint,
-  httpLibEndpoint: environmentData.httpLibEndpoint,
-  serverEndpoint: environmentData.serverEndpoint,
-  wsApiEndpoint: environmentData.wsApiEndpoint,
+  httpApiEndpoint: environmentData.httpApiEndpoint.replace('localhost', location.hostname),
+  httpLibEndpoint: environmentData.httpLibEndpoint.replace('localhost', location.hostname),
+  serverEndpoint: environmentData.serverEndpoint.replace('localhost', location.hostname),
+  wsApiEndpoint: environmentData.wsApiEndpoint.replace('localhost', location.hostname),
   defaultQuizSettings: {
     answers: {
       answerText: '',
@@ -15,7 +15,7 @@ export const DefaultSettings = {
       configCaseSensitive: false,
       configTrimWhitespaces: false,
       configUseKeywords: false,
-      configUsePunctuation: false
+      configUsePunctuation: false,
     },
     question: {
       dispayAnswerText: true,
@@ -26,26 +26,26 @@ export const DefaultSettings = {
       rangeMin: 0,
       rangeMax: 60,
       correctValue: 30,
-      answerOptionList: []
+      answerOptionList: [],
     },
     music: {
       enabled: {
         lobby: true,
         countdownRunning: true,
-        countdownEnd: true
+        countdownEnd: true,
       },
       volumeConfig: {
         global: 60,
         lobby: 60,
         countdownRunning: 60,
         countdownEnd: 60,
-        useGlobalVolume: true
+        useGlobalVolume: true,
       },
       titleConfig: {
         lobby: 'Song0',
         countdownRunning: 'Song0',
-        countdownEnd: 'Song0'
-      }
+        countdownEnd: 'Song0',
+      },
     },
     nicks: {
       memberGroups: ['Default'],
@@ -53,12 +53,12 @@ export const DefaultSettings = {
       autoJoinToGroup: false,
       blockIllegalNicks: true,
       restrictToCasLogin: false,
-      selectedNicks: []
+      selectedNicks: [],
     },
     theme: 'theme-Material',
     readingConfirmationEnabled: true,
     showResponseProgress: true,
     confidenceSliderEnabled: true,
-    cacheQuizAssets: false
-  }
+    cacheQuizAssets: false,
+  },
 };
