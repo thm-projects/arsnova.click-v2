a53c16288cb012d85985c10a331f00c5e409c2fd
Christopher Fullarton <christopher@fullarton.eu>
2018-07-30 19:43:15 +0200

    Fixes tests

--- src/lib/jwt.factory.ts
+++ src/lib/jwt.factory.ts
@@ -1,6 +1,6 @@
 import { DB_TABLE, STORAGE_KEY } from '../app/shared/enums';
 
-export function jwtOptionsFactory(storageService) {
+export function jwtOptionsFactory(storageService): any {
   return {
     tokenGetter: async () => {
       const tokens = await storageService.read(DB_TABLE.CONFIG, STORAGE_KEY.TOKEN).toPromise();
