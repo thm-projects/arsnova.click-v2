4e65fb2ab0864f69c66b721f995cba95db1c4fd3
Christopher Fullarton <christopher@fullarton.eu>
2019-04-08 14:56:44 +0200

    Fixes build problems

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -13,34 +13,25 @@
  *
  * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
  */
+
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-/* IE9, IE10 and IE11 requires all of the following polyfills. **/
-// import 'core-js/es6/symbol';
-// import 'core-js/es6/object';
-// import 'core-js/es6/function';
-// import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
-// import 'core-js/es6/math';
-// import 'core-js/es6/string';
-// import 'core-js/es6/date';
-// import 'core-js/es6/array';
-// import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
-// import 'core-js/es6/weak-map';
-// import 'core-js/es6/set';
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js'; // Run `npm install --save classlist.js`.
-/* Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
 
 /**
- * Required to support Web Animations `@angular/animation`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
+ * INTERNET EXPLORER
+ *
+ * All the Internet Explorer polyfills are defined separately, in the `ie-polyfills.js` file.
+ * They are also built separately from the main app, via the `yarn build-ie-polyfills` task.
+ * The output of this build is conditionally loaded (only if we are running IE) in the browser,
+ * and executed before the rest of the application files are executed.
  */
+
+/* ALL Firefox browsers require the following to support `@angular/animation`. **/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+
+import '@webcomponents/custom-elements'; //  Custom Elements Polyfill
+import '@webcomponents/custom-elements/src/native-shim';
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
@@ -56,7 +47,3 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 // import 'intl';  // Run `npm install --save intl`.
-/**
- * Need to import at least one locale-data with intl.
- */
-// import 'intl/locale-data/jsonp/en';
