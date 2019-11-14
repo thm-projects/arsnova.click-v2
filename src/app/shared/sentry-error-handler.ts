ccffb2d0635685f79d89808cdeb04c59bc9026fc
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2019-11-14 09:43:48 +0100

    Fixes check for prod environment and version of the sentry package in the package.json

--- src/app/shared/sentry-error-handler.ts
+++ src/app/shared/sentry-error-handler.ts
@@ -2,7 +2,7 @@ import { ErrorHandler, Injectable } from '@angular/core';
 import { captureException, init as SentryInit, showReportDialog } from '@sentry/browser';
 import { environment } from '../../environments/environment';
 
-if (!environment.production) {
+if (environment.production) {
   SentryInit({
     dsn: 'https://f16c02fdefe64c018f5d580d1cf05b56@sentry.io/1819496',
   });
