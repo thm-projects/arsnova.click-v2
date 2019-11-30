ad33f6f954ef56e7684f8569e633cc0ce035f6f7
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2019-11-30 11:37:27 +0100

    Fixes logout if reloading the page where authentication is required

--- src/app/service/login/static-login.service.ts
+++ src/app/service/login/static-login.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Route, Router, UrlSegment } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { UserRole } from '../../lib/enums/UserRole';
 import { UserService } from '../user/user.service';
 
@@ -10,33 +12,40 @@ export class StaticLoginService implements CanLoad {
 
   constructor(private router: Router, private userService: UserService) { }
 
-  public canLoad(route: Route, segments: UrlSegment[]): boolean {
-    if (this.isAllowedToProceed(route)) {
-      return true;
-    }
+  public canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> {
+    return new Observable<boolean>(subscriber => {
+      this.isAllowedToProceed(route).pipe(map(isAllowedToProceed => {
+        if (isAllowedToProceed) {
+          return true;
+        }
 
-    this.router.navigate(['/login'], {
-      queryParams: {
-        return: segments.map(segment => segment.path).join('/'),
-      },
+        this.router.navigate(['/login'], {
+          queryParams: {
+            return: segments.map(segment => segment.path).join('/'),
+          },
+        });
+        return false;
+      })).subscribe(isAllowedToProceed => {
+        subscriber.next(isAllowedToProceed);
+        subscriber.complete();
+      });
     });
-    return false;
   }
 
-  private isAllowedToProceed(route: Route): boolean {
-    if (!this.userService.isLoggedIn) {
-      return false;
-    }
+  private isAllowedToProceed(route: Route): Observable<boolean> {
+    return this.userService.loginNotifier.pipe(map(isLoggedIn => {
+      if (!isLoggedIn) {
+        return false;
+      }
 
-    switch (route.path) {
-      case 'i18n-manager':
-        return this.userService.isAuthorizedFor(UserRole.EditI18n);
-      case 'quiz-manager':
-        return this.userService.isAuthorizedFor(UserRole.CreateExpiredQuiz);
-      case 'admin':
-        return this.userService.isAuthorizedFor([UserRole.QuizAdmin, UserRole.SuperAdmin]);
-      default:
-        return true;
-    }
+      switch (route.path) {
+        case 'i18n-manager':
+          return this.userService.isAuthorizedFor(UserRole.EditI18n);
+        case 'admin':
+          return this.userService.isAuthorizedFor([UserRole.QuizAdmin, UserRole.SuperAdmin]);
+        default:
+          return true;
+      }
+    }));
   }
 }
