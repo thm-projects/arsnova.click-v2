9f8ffa5531c992a2712a0b783d4179fe20b22642
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2019-12-03 19:13:01 +0100

    Optimizes setting of the document title

--- src/app/service/header-label/header-label.service.ts
+++ src/app/service/header-label/header-label.service.ts
@@ -1,5 +1,5 @@
-import { isPlatformBrowser } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
@@ -16,16 +16,16 @@ export class HeaderLabelService {
 
   set headerLabel(value: string) {
     this._headerLabel = value;
-    if (isPlatformBrowser(this.platformId)) {
-      if (value === 'default') {
-        document.title = 'arsnova.click';
-      } else {
-        this.translateService.get(value).subscribe(translatedValue => document.title = 'arsnova.click - ' + translatedValue);
-      }
+    if (!value || value === 'default') {
+      this.titleService.setTitle('arsnova.click');
+    } else {
+      this.translateService.get(value).subscribe(translatedValue => {
+        this.titleService.setTitle('arsnova.click - ' + translatedValue);
+      });
     }
   }
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object, private translateService: TranslateService) {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private translateService: TranslateService, private titleService: Title) {
     this.translateService.onLangChange.subscribe(() => {
       this.headerLabel = this._headerLabel;
     });
