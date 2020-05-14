b86cbb0f42a2e52ac295d8e3e713542de6372628
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2020-05-14 11:02:44 +0200

    Fixes tests

--- src/app/quiz/quiz-flow/quiz-results/modals/bonus-token/bonus-token.component.spec.ts
+++ src/app/quiz/quiz-flow/quiz-results/modals/bonus-token/bonus-token.component.spec.ts
@@ -1,102 +1,103 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PLATFORM_ID } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { BonusTokenComponent } from './bonus-token.component';
-import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
-import {HttpClient, HttpHandler} from '@angular/common/http';
-import {JWT_OPTIONS, JwtHelperService, JwtModule} from '@auth0/angular-jwt';
-import {jwtOptionsFactory} from '../../../../../lib/jwt.factory';
-import {PLATFORM_ID} from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { JWT_OPTIONS, JwtHelperService, JwtModule } from '@auth0/angular-jwt';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import {
   TranslateCompiler,
   TranslateFakeCompiler,
   TranslateFakeLoader,
   TranslateLoader,
-  TranslateParser, TranslatePipe,
+  TranslateParser,
+  TranslatePipe,
   TranslateService,
-  TranslateStore
+  TranslateStore,
 } from '@ngx-translate/core';
-import {TranslateServiceMock} from '../../../../../../_mocks/_services/TranslateServiceMock';
-import {TranslatePipeMock} from '../../../../../../_mocks/_pipes/TranslatePipeMock';
-import {I18nTestingModule} from '../../../../../shared/testing/i18n-testing/i18n-testing.module';
-import {SharedModule} from '../../../../../shared/shared.module';
-import {RouterTestingModule} from '@angular/router/testing';
-import {RxStompRPCService, RxStompService} from '@stomp/ng2-stompjs';
-import {MarkdownService, MarkedOptions} from 'ngx-markdown';
-import {StorageService} from '../../../../../service/storage/storage.service';
-import {StorageServiceMock} from '../../../../../service/storage/storage.service.mock';
-import {FooterBarService} from '../../../../../service/footer-bar/footer-bar.service';
-import {SettingsService} from '../../../../../service/settings/settings.service';
-import {ConnectionService} from '../../../../../service/connection/connection.service';
-import {ConnectionMockService} from '../../../../../service/connection/connection.mock.service';
-import {QuizService} from '../../../../../service/quiz/quiz.service';
-import {QuizMockService} from '../../../../../service/quiz/quiz-mock.service';
-import {SharedService} from '../../../../../service/shared/shared.service';
-import {AttendeeService} from '../../../../../service/attendee/attendee.service';
-import {AttendeeMockService} from '../../../../../service/attendee/attendee.mock.service';
+import { RxStompRPCService, RxStompService } from '@stomp/ng2-stompjs';
+import { MarkdownService, MarkedOptions } from 'ngx-markdown';
+import { TranslatePipeMock } from '../../../../../../_mocks/_pipes/TranslatePipeMock';
+import { TranslateServiceMock } from '../../../../../../_mocks/_services/TranslateServiceMock';
+import { jwtOptionsFactory } from '../../../../../lib/jwt.factory';
+import { AttendeeMockService } from '../../../../../service/attendee/attendee.mock.service';
+import { AttendeeService } from '../../../../../service/attendee/attendee.service';
+import { ConnectionMockService } from '../../../../../service/connection/connection.mock.service';
+import { ConnectionService } from '../../../../../service/connection/connection.service';
+import { FooterBarService } from '../../../../../service/footer-bar/footer-bar.service';
+import { QuizMockService } from '../../../../../service/quiz/quiz-mock.service';
+import { QuizService } from '../../../../../service/quiz/quiz.service';
+import { SettingsService } from '../../../../../service/settings/settings.service';
+import { SharedService } from '../../../../../service/shared/shared.service';
+import { StorageService } from '../../../../../service/storage/storage.service';
+import { StorageServiceMock } from '../../../../../service/storage/storage.service.mock';
+import { SharedModule } from '../../../../../shared/shared.module';
+import { I18nTestingModule } from '../../../../../shared/testing/i18n-testing/i18n-testing.module';
+
+import { BonusTokenComponent } from './bonus-token.component';
 
 describe('When BonusToken Icon is going to be clicked', () => {
-    let component: BonusTokenComponent;
-    let fixture: ComponentFixture<BonusTokenComponent>;
+  let component: BonusTokenComponent;
+  let fixture: ComponentFixture<BonusTokenComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        I18nTestingModule, SharedModule, RouterTestingModule, JwtModule.forRoot({
-          jwtOptionsProvider: {
-            provide: JWT_OPTIONS,
-            useFactory: jwtOptionsFactory,
-            deps: [PLATFORM_ID],
-          }
-        })
-      ],
-      declarations: [ BonusTokenComponent ],
-      providers: [
-          NgbActiveModal, HttpClient, HttpHandler, JwtHelperService,
-        {
-          provide: TranslateService,
-          useClass: TranslateServiceMock
-        }, TranslateStore, {
-          provide: TranslateLoader,
-          useClass: TranslateFakeLoader
-        }, {
-          provide: TranslateCompiler,
-          useClass: TranslateFakeCompiler
-        }, TranslateParser, {
-          provide: TranslatePipe,
-          useClass: TranslatePipeMock
-        }, RxStompRPCService, MarkdownService, {
-          provide: MarkedOptions,
-          useValue: {},
-        }, RxStompService, {
-          provide: StorageService,
-          useClass: StorageServiceMock,
-        }, TranslateService, FooterBarService, SettingsService, {
-          provide: ConnectionService,
-          useClass: ConnectionMockService,
-        }, {
-          provide: QuizService,
-          useClass: QuizMockService,
-        }, SharedService, {
-          provide: AttendeeService,
-          useClass: AttendeeMockService,
-        },
-      ]
-    })
-    .compileComponents();
+        imports: [
+          HttpClientTestingModule, I18nTestingModule, SharedModule, RouterTestingModule, JwtModule.forRoot({
+            jwtOptionsProvider: {
+              provide: JWT_OPTIONS,
+              useFactory: jwtOptionsFactory,
+              deps: [PLATFORM_ID],
+            }
+          })
+        ],
+        declarations: [ BonusTokenComponent ],
+        providers: [
+          NgbActiveModal, JwtHelperService,
+          {
+            provide: TranslateService,
+            useClass: TranslateServiceMock
+          }, TranslateStore, {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader
+          }, {
+            provide: TranslateCompiler,
+            useClass: TranslateFakeCompiler
+          }, TranslateParser, {
+            provide: TranslatePipe,
+            useClass: TranslatePipeMock
+          }, RxStompRPCService, MarkdownService, {
+            provide: MarkedOptions,
+            useValue: {},
+          }, RxStompService, {
+            provide: StorageService,
+            useClass: StorageServiceMock,
+          }, TranslateService, FooterBarService, SettingsService, {
+            provide: ConnectionService,
+            useClass: ConnectionMockService,
+          }, {
+            provide: QuizService,
+            useClass: QuizMockService,
+          }, SharedService, {
+            provide: AttendeeService,
+            useClass: AttendeeMockService,
+          },
+        ]
+      })
+      .compileComponents();
   }));
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(BonusTokenComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BonusTokenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should be created', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
 
-    it('should change value of the Copy-to-clipboard-Button after copying to clipboard', () => {
-        component.copy();
-        expect(component.clipboardText).toBeFalsy();
-    });
+  it('should change value of the Copy-to-clipboard-Button after copying to clipboard', () => {
+    component.copy();
+    expect(component.clipboardText).toBeFalsy();
+  });
 });
