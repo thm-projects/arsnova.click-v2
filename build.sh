cc5b7fe4dbfb72c574a5cac01164a1df7bc1780d
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2020-05-21 15:59:03 +0200

    Fixes css hash map

--- build.sh
+++ build.sh
@@ -12,6 +12,19 @@ npm run build:"$1"
 echo "Copying assets"
 npm run copy:"$1"
 
+# Renaming css files to a hashed version and add a json file containing the theme and the corresponding hash
+cd /usr/src/app/dist/frontend/browser
+echo "Building css file hashes"
+sha1sum theme-*.css > theme-hashes.txt
+echo "Renaming the css files with their hash"
+while read p; do
+  hash=$(echo "$p" | cut -d ' ' -f1)
+  file=$(echo "$p" | cut -d ' ' -f3)
+  mv $file "$(echo "$file" | sed s/%%_CSS_FILE_HASH_%%/$hash/)"
+done < theme-hashes.txt
+echo "Building json file which contains a map with the theme name and the current hash"
+jq -R -s -c 'split("\n") | .[] | split(" ") | {hash: .[0], theme: .[2] | rtrimstr("\n") | sub("\\-%%_CSS_FILE_HASH_%%";"") | sub("theme-";"")}' < theme-hashes.txt > assets/theme-hashes.json
+
 cd /usr/src/app/dist/frontend/
 echo "Starting the http server"
 angular-http-server --path browser/ --silent -p 4711 &
@@ -30,16 +43,6 @@ cd /usr/src/app
 # Disabled for now since purifyCSS removes nearly all css with Angular 9
 # echo "Purifying css"
 # npm run purify
-
-# Renaming css files to a hashed version and add a json file containing the theme and the corresponding hash
-cd /usr/src/app/dist/frontend/browser
-sha1sum theme-*.css > theme-hashes.txt
-while read p; do
-  hash=$(echo "$p" | cut -d ' ' -f1)
-  file=$(echo "$p" | cut -d ' ' -f3)
-  mv $file "$(echo "$file" | sed s/%%_CSS_FILE_HASH_%%/$hash/)"
-done < theme-hashes.txt
-jq -R -s -c 'split(" ") | {hash: .[0],theme: .[2] | rtrimstr("\n") | sub("\\-%%_CSS_FILE_HASH_%%";"") | sub("theme-";"")}' < theme-hashes.txt > assets/theme-hashes.json
 
 echo "Gzipping app files"
 find . -name "*.*" -type f -print0 | xargs -0 gzip -9 -k
