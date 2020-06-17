69419eb4923a95603b39965ed4a2e39b0ccdd4ba
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2020-06-17 18:39:07 +0200

    Fixes tests

--- src/app/modals/outdated-version/outdated-version.component.spec.ts
+++ src/app/modals/outdated-version/outdated-version.component.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SwUpdate } from '@angular/service-worker';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { SwUpdateMock } from '../../../_mocks/_services/SwUpdateMock';
 import { I18nTestingModule } from '../../shared/testing/i18n-testing/i18n-testing.module';
 
 import { OutdatedVersionComponent } from './outdated-version.component';
@@ -11,7 +14,11 @@ describe('OutdatedVersionComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [I18nTestingModule],
-      providers: [NgbActiveModal],
+      providers: [
+        NgbActiveModal,
+        {provide: SwUpdate, useClass: SwUpdateMock},
+        {provide: ToastrService, useValue: {}},
+      ],
       declarations: [ OutdatedVersionComponent ]
     })
     .compileComponents();
