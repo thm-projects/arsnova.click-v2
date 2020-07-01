32abe702be48d05d43387f39bceaa83cbee4cc2d
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2020-07-01 08:37:23 +0200

    captures exceptions silently

--- src/app/shared/sentry-error-handler.ts
+++ src/app/shared/sentry-error-handler.ts
@@ -1,6 +1,5 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { ErrorHandler, Injectable } from '@angular/core';
-import { captureException, Event as SentryEvent, init as SentryInit, setExtra, showReportDialog } from '@sentry/browser';
+import { captureException, Event as SentryEvent, init as SentryInit, setExtra } from '@sentry/browser';
 import { EventHint } from '@sentry/types';
 import { environment } from '../../environments/environment';
 
@@ -9,7 +8,7 @@ export class SentryErrorHandler implements ErrorHandler {
   private readonly _enabled = !!environment.sentryDSN;
 
   constructor() {
-    if (!environment.sentryDSN) {
+    if (!this._enabled) {
       return;
     }
 
@@ -30,12 +29,7 @@ export class SentryErrorHandler implements ErrorHandler {
   }
 
   public handleError(error): void {
-    if (!this._enabled || error instanceof HttpErrorResponse) {
-      return;
-    }
-
-    console.log('error occured', error);
-    const eventId = captureException(error.originalError || error);
-    showReportDialog({ eventId });
+    captureException(error.originalError || error);
+    throw error;
   }
 }
