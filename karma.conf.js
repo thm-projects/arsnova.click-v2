d0228bd8b2f8afa729a29e521cf2036869166750
Christopher Fullarton <christopher.fullarton@mni.thm.de>
2020-11-14 10:39:05 +0100

    Fixes deprecations for unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -10,14 +10,14 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-mocha-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
       captureConsole: false
     },
-    coverageIstanbulReporter: {
+    coverage: {
       dir: require('path').join(__dirname, 'coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
