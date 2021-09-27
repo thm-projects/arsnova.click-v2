aea8aed6bdcc80b08226d3ee06cafacfa1344db1
Philipp Sautner <philipp.sautner@mni.thm.de>
2021-09-27 12:18:43 +0200

    Revert "Removes fullscreen option"
    This reverts commit a6757c8c4653b8baabf2c7ea54511a8d093d8678.

--- src/app/root/home/home.component.ts
+++ src/app/root/home/home.component.ts
@@ -409,6 +409,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       this.footerBarService.footerElemAbout,
       this.footerBarService.footerElemTranslation,
       this.footerBarService.footerElemTheme,
+      this.footerBarService.footerElemFullscreen,
     ];
 
     if (environment.enableQuizPool) {
