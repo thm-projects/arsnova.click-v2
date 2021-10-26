5680f74dd689eb1535ae00738e8742d4f7d49740
Philipp Sautner <philipp.sautner@mni.thm.de>
2021-10-26 10:18:23 +0200

    Changes route to staging.frag.jetzt

---
+++ src/app/service/local-storage/local-storage-receive.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageReceiveService {
+
+  constructor() {
+    //Pings parent window and asks for response with key and value of "Language"
+    window.parent.postMessage({
+      action: 'get',
+      key: 'Language'
+    }, 'https://staging.frag.jetzt');
+
+    window.parent.postMessage({
+      action: 'get',
+      key: 'Theme'
+    }, 'https://staging.frag.jetzt');
+
+    window.addEventListener('message', this.messageHandler, false);
+  }
+
+
+  messageHandler(event) {
+    const { action, key, value } = event.data;
+    if ( action == 'returnData') {
+      this.useData(key, value)
+    }
+  }
+
+  useData(key, value) {
+    console.log(key); //TODO: erase
+    window.localStorage.setItem(key, value);
+  }
+
+  //TODO: Listener on localStorage language and localstorage Theme which react when theyre being changed
+}
